5623c0dc1adeceda0a3a597c18cf6ebdcf9a6643
KxxgHx <amadona03@naver.com>
2025-10-02 12:57:39 +0900

    Java HW

--- Java/LSP_Fix.java
+++ Java/LSP_Fix.java
@@ -1,4 +1,4 @@
-class Rectangle {
+class Rectangle_Fix {
     protected int width, height;
 
     void setWidth(int w) {
@@ -16,39 +16,42 @@ class Rectangle {
     int getHeight() {
         return height;
     }
-}
 
-class Square_Fix {
-    final private Rectangle rect;
 
-    public Square_Fix() {
-        rect = new Rectangle();
-    }
+    static class Square_Fix {
+        private final Rectangle_Fix rect;
 
-    void setWidth(int w) {
-        rect.setWidth(w);
-        rect.setHeight(w);
-    }
+        public Square_Fix() {
+            rect = new Rectangle_Fix();
+        }
 
-    int getWidth() {
-        return rect.getWidth();
-    }
+        void setSize(int size) {
+            rect.setWidth(size);
+            rect.setHeight(size);
+        }
 
-    int getHeight() {
-        return rect.getHeight();
+        int getWidth() {
+            return rect.getWidth();
+        }
+
+        int getHeight() {
+            return rect.getHeight();
+        }
     }
 }
 
 public class LSP_Fix {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle();
-        Square_Fix r2 = new Square_Fix();
+        Rectangle_Fix r = new Rectangle_Fix();
+        Rectangle_Fix.Square_Fix s = new Rectangle_Fix.Square_Fix();
 
         r.setWidth(10);
         r.setHeight(5);
-        r2.setWidth(10);
+
+        s.setSize(10);
 
         System.out.println("Rectangle's Size : Width " + r.getWidth() + ", Height " + r.getHeight());
-        System.out.println("Square's Size : Width " + r2.getWidth() + ", Height " + r2.getHeight());
+        System.out.println("Square's Size : Width " + s.getWidth() + ", Height " + s.getHeight());
     }
 }
+
