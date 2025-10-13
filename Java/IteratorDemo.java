a521f54e3c19714fa955ffadb9dfc6d77f790f4f
KxxgHx <amadona03@naver.com>
2025-10-13 16:01:47 +0900

    Java Practice

--- Java/IteratorDemo.java
+++ Java/IteratorDemo.java
@@ -9,11 +9,11 @@ public class IteratorDemo {
             System.out.print(iterator.next() + ",");
         System.out.println();
 
-        while(iterator.hasNext())
+        while(iterator.hasNext()) //이미 순회를 하였기에 empty 상태가 됨
             System.out.print(iterator.next() + ",");
         System.out.println();
 
-        iterator = list.iterator();
+        iterator = list.iterator(); //iterator을 재설정하여 다시 채움
         while(iterator.hasNext())
             System.out.print(iterator.next() + ",");
         System.out.println();
