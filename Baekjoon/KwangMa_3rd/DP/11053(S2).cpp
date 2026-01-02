13a8e9d76b98b01d7324542aea9ac726862801e4
KxxgHx <amadona03@naver.com>
2026-01-02 18:18:19 +0900

    add problem

---
+++ Baekjoon/KwangMa_3rd/DP/11053(S2).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int A[1001];
+int dp[1001];
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    int result = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+        
+        dp[i] = 1; 
+
+        for (int j = 0; j < i; j++)
+        {
+            if (A[j] < A[i])
+            {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        
+        if (result < dp[i])
+            result = dp[i];
+    }
+
+    cout << result;
+}
---
+++ Baekjoon/KwangMa_3rd/Tree/1991(S1).cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+    char left;
+    char right;
+};
+
+Node tree[27];
+
+void preOrder(char node) {
+    if (node == '.') return;
+    
+    cout << node;                 
+    preOrder(tree[node - 'A'].left);
+    preOrder(tree[node - 'A'].right);
+}
+
+void inOrder(char node) {
+    if (node == '.') return;
+
+    inOrder(tree[node - 'A'].left);
+    cout << node;      
+    inOrder(tree[node - 'A'].right);
+}
+
+void postOrder(char node) {
+    if (node == '.') return;
+
+    postOrder(tree[node - 'A'].left);
+    postOrder(tree[node - 'A'].right);
+    cout << node;                 
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    char root, l, r;
+    for (int i = 0; i < N; i++) {
+        cin >> root >> l >> r;
+        tree[root - 'A'].left = l;
+        tree[root - 'A'].right = r;
+    }
+
+    preOrder('A');
+    cout << "\n";
+    
+    inOrder('A');
+    cout << "\n";
+    
+    postOrder('A');
+    cout << "\n";
+
+    return 0;
+}
