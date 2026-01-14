c5d1deeba76338a5aad147010d767ca580489cea
KxxgHx <amadona03@naver.com>
2026-01-14 14:26:28 +0900

    add

---
+++ Baekjoon/KwangMa_4th/Dynamic_Programming/9251(G5).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string str1, str2;
+    cin >> str1 >> str2;
+    int a = str1.size();
+    int b = str2.size();
+    vector<vector<int>> dp(a + 1, vector<int>(b + 1, 0));
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= b; j++)
+        {
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+
+    }
+    cout << dp[a][b] << '\n';
+
+    
+}
