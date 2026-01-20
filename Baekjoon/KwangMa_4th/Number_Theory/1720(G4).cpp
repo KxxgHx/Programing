c945687392a6fc66625bbaee23a616a16d0e4dae
KxxgHx <amadona03@naver.com>
2026-01-20 18:20:47 +0900

    add

---
+++ Baekjoon/KwangMa_4th/Number_Theory/1720(G4).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N;
+int dp[31];
+
+int main()
+{
+    cin >> N;
+
+    dp[0] = dp[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] * 2;
+
+    if (N % 2)
+        cout << (dp[N] + dp[N / 2]) / 2 << "\n";
+    else
+        cout << (dp[N] + dp[N / 2] + 2 * dp[N / 2 - 1]) / 2 << "\n";
+}
