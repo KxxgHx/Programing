c6ed9eceb726db96a1d049e6243499530a9765da
KxxgHx <amadona03@naver.com>
2026-01-28 15:43:56 +0900

    add

---
+++ Baekjoon/KwangMa_4th/Back_Tracking/18429(S3).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int ans;
+int n, k;
+
+vector <int> kit;
+bool visited[8];
+
+void dfs(int cnt, int total) {
+	if (total < 500)
+		return;
+	if (cnt == kit.size()) {
+		ans++;
+		return;
+	}
+
+	for (int i = 0; i < kit.size(); i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			dfs(cnt + 1, total + kit[i] - k);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		int c; cin >> c;
+		kit.push_back(c);
+	}
+
+	dfs(0, 500);
+
+	cout << ans;
+}
---
+++ Baekjoon/KwangMa_4th/Tree_and_Graph/24480(S2).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, R;
+vector<int> graph[100001];
+int visited[100001];
+int cnt = 0;
+
+void dfs(int current){
+    visited[current] = ++cnt;
+    
+    for(int next : graph[current]){
+        if(!visited[next])
+            dfs(next);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N >> M >> R;
+
+    for (int i = 0; i < M; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= N; i++)
+    {
+        sort(graph[i].begin(), graph[i].end(),greater<>());
+    }
+    
+    dfs(R);
+    
+    for (int i = 1; i <= N; i++)
+    {
+        cout << visited[i] << '\n';
+    }
+    
+    return 0;
+}
