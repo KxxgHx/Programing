04e45c2ccf9d919fe1069090ab4e814041763c1f
KxxgHx <amadona03@naver.com>
2026-02-18 13:46:44 +0900

    add

---
+++ Baekjoon/KwangMa_4th/Binary_Search/2295(G4).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> sum;
+int arr[1001];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            sum.push_back(arr[i] + arr[j]);
+        }
+    }
+
+    sort(arr, arr + N);
+    sort(sum.begin(), sum.end());
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            int mid = arr[i] - arr[j];
+            bool check = binary_search(sum.begin(), sum.end(), mid);
+            if(check)
+            {
+                cout << arr[i] << '\n';
+                return 0;
+            }
+            
+        }
+    }
+}
